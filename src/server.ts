9e4f46884b839e7f3c043f5938c4aa5fd21abfaf
ericoBandeira <ericobandeira@hotmail.com>
2021-06-21 23:58:35 -0300

    adicionando migrations

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import "reflect-metadata";
+
+import "./database";
 
 //@types/express
 const app = express();
@@ -9,15 +12,24 @@ const app = express();
 // DELETE=> Remover uma informação
 // PATCH=> Alterar uma informação específica
 
-app.get("/test", (request, response)=>{
-    //Request => Entrando
-    //Response => Saindo
-    return response.send("óla, NLW")
-})
+//Tipos de parâmetros:
+// Routes params => parametros que fazem partes da rota ex: http://localhost:3000/products/124801924
+// Query params => parametros que fazm parte de uma query ex: http://localhost:3000/products?name=teclado&description=tecladobom (não obrigatórios)
+// Body params => parametros do corpo da requisição ex:
+// {
+//     "name": "telcado",
+//     "description": "teclado bom"
+// }
 
-app.post("/test-post",(request, response)=>{
-    return response.send("óla, NLW post")
-})
+// app.get("/test", (request, response)=>{
+//     //Request => Entrando
+//     //Response => Saindo
+//     return response.send("óla, NLW")
+// })
+
+// app.post("/test-post",(request, response)=>{
+//     return response.send("óla, NLW post")
+// })
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Server is running"))
